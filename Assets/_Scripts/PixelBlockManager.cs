7f8d69c5ee246927bfeaaf30a603b7043ef9a84e
TLeTu <tranletu2003@gmail.com>
2026-04-17 22:54:58 +0700

    PixelBlockManager: Initialize the pixel grid

--- Assets/_Scripts/PixelBlockManager.cs
+++ Assets/_Scripts/PixelBlockManager.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class PixelObjectManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject pixelPrefab;
+    private int width;
+    private int height;
+    private bool[,] grid;
+    private GameObject[,] pixelObjects;
+    private void Start()
+    {
+        Initialize(10, 10);
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize(int w, int h)
     {
-        
+        width = w;
+        height = h;
+        grid = new bool[width, height];
+        pixelObjects = new GameObject[width, height];
+        float offsetX = (width - 1) / 2f;
+        float offsetY = (height - 1) / 2f;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 position = new Vector3(x - offsetX, y - offsetY, 0);
+                GameObject pixel = Instantiate(pixelPrefab, Vector3.zero, Quaternion.identity, transform);
+                pixel.transform.SetParent(transform, false);
+                pixel.transform.localPosition = position;
+                pixelObjects[x, y] = pixel;
+                grid[x, y] = true;
+            }
+        }
     }
 }
