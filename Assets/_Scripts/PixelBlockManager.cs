f8fedf91c5f1e8f7bfa3d1738373952b7bac5645
TLeTu <tranletu2003@gmail.com>
2026-04-18 12:16:27 +0700

    PixelBlockManager: When the block is split, flood fill find the chunk and create a new pixel block from it

--- Assets/_Scripts/PixelBlockManager.cs
+++ Assets/_Scripts/PixelBlockManager.cs
@@ -36,13 +36,34 @@ public class PixelBlockManager : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 GameObject pixel = Instantiate(pixelPrefab, transform);
-                float z = UnityEngine.Random.Range(-0.5f, 0.5f);
-                pixel.transform.localPosition = new Vector3(x - offsetX, y - offsetY, z);
+                // float z = UnityEngine.Random.Range(-0.5f, 0.5f);
+                pixel.transform.localPosition = new Vector3(x - offsetX, y - offsetY);
                 pixelObjects[x, y] = pixel;
                 grid[x, y] = true;
             }
         }
     }
+
+    public void InitiateEmptyBlock(int w, int h)
+    {
+        width = w;
+        height = h;
+
+        grid = new bool[width, height];
+        pixelObjects = new GameObject[width, height];
+    }
+
+    public void AddPixel(int x, int y, GameObject pixel)
+    {
+        grid[x, y] = true;
+        pixelObjects[x, y] = pixel;
+
+        pixel.transform.SetParent(transform);
+        float offsetX = (width - 1f) / 2f;
+        float offsetY = (height - 1f) / 2f;
+
+        pixel.transform.localPosition = new Vector2(x - offsetX, y - offsetY);
+    }   
 
     public void HitAtPoint(Vector2 worldHitPoint)
     {
@@ -87,6 +108,8 @@ public class PixelBlockManager : MonoBehaviour
         Rigidbody2D rb = pixel.AddComponent<Rigidbody2D>();
         float randomX = UnityEngine.Random.Range(-2f, 2f);
         rb.AddForce(new Vector2(randomX, 5f), ForceMode2D.Impulse);
+
+        CheckSplitChunks();
     }
 
     private void CheckSplitChunks()
@@ -108,7 +131,50 @@ public class PixelBlockManager : MonoBehaviour
         }
         if (chunks.Count > 1)
         {
-            // Later
+            chunks.Sort((a, b) => b.Count.CompareTo(a.Count));
+            for (int i = 1; i < chunks.Count; i++)
+            {
+                CreateChunk(chunks[i]);
+            }
+        }
+    }
+
+    private void CreateChunk(List<Vector2Int> chunkPixels)
+    {
+        int minX = int.MaxValue, maxX = int.MinValue, minY = int.MaxValue, maxY = int.MinValue;
+        foreach (var pos in chunkPixels)
+        {
+            minX = Math.Min(minX, pos.x);
+            maxX = Math.Max(maxX, pos.x);
+            minY = Math.Min(minY, pos.y);
+            maxY = Math.Max(maxY, pos.y);
+        }
+        
+        float oldOffSetX = (width - 1f) / 2f;
+        float oldOffSetY = (height - 1f) / 2f;
+
+        float chunkCenterX = (minX + maxX) / 2f;
+        float chunkCenterY = (minY + maxY) / 2f;
+
+        Vector2 chunkCenterLocalPos = new Vector2(chunkCenterX - oldOffSetX, chunkCenterY - oldOffSetY);
+        Vector2 chunkCenterWorldPos = transform.TransformPoint(chunkCenterLocalPos);
+
+        int newWidth = maxX - minX + 1;
+        int newHeight = maxY - minY + 1;
+
+        GameObject chunkObj = Instantiate(chunkPrefab, chunkCenterWorldPos, Quaternion.identity);
+        chunkObj.GetComponent<PixelBlockManager>().InitiateEmptyBlock(newWidth, newHeight);
+
+        foreach (var pos in chunkPixels)
+        {
+            GameObject pixel = pixelObjects[pos.x, pos.y];
+            if (pixel != null)
+            {
+                int newX = pos.x - minX;
+                int newY = pos.y - minY;
+                chunkObj.GetComponent<PixelBlockManager>().AddPixel(newX, newY, pixel);
+                pixelObjects[pos.x, pos.y] = null;
+            }
         }
     }
 
